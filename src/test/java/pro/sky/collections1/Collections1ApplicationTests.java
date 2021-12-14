52f26e1e28430325fc2ae6ba2e813c44dda427d9
Igor <avt191540@yandex.ru>
2021-12-14 09:19:41 +0300

    Initial commit

---
+++ src/test/java/pro/sky/collections1/Collections1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.collections1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Collections1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
